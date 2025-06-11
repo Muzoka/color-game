1ba96f875fca37b0d1129311f6c41e848c1ab177
Muzoka <abdullah.ah.alqurashi@gmail.com>
2025-06-11 14:09:18 +0300

    refactor: load firebase config from module

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBRz_YHWRurV0ZazilVra-chyp70CXyuIY",
+  authDomain: "color-war-4b8ee.firebaseapp.com",
+  projectId: "color-war-4b8ee",
+  storageBucket: "color-war-4b8ee.appspot.com",
+  messagingSenderId: "987787983661",
+  appId: "1:987787983661:web:3fcfe121bebb6f4b7f6b1a",
+  measurementId: "G-YNHTF1VLXF"
+};
+
